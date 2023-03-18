ccac12966bc51ec47bb976809e2b8f4bf1d7fb7a
Rostyslav Mysiuha <rossmysiuga@gmail.com>
2023-03-18 12:41:39 -0400

    Pricing section Heading and switcher

--- src/js/main.js
+++ src/js/main.js
@@ -166,4 +166,24 @@ function accordion() {
         })
     })
 }
-accordion() 
+accordion()
+
+// Switcher
+function togglePrice(){
+    const billing = document.querySelector('.billing')
+    const billingItems = document.querySelectorAll('.billing__item')
+
+    billing.addEventListener('click', () => {
+        billing.classList.toggle('active')
+
+        if(!billing.classList.contains('active')){
+            billingItems[0].classList.add('active')
+            billingItems[2].classList.remove('active')
+        } else {
+            billingItems[2].classList.add('active')
+            billingItems[0].classList.remove('active')
+        }
+    })
+}
+
+togglePrice()
